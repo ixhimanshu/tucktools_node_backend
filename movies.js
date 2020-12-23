074d441ef329b2274b3cb0940be5091ab6aa1231
Himanshu <himanshu.thakur@mobicules.com>
2020-12-23 19:23:40 +0530

    --linkedin video downloader added

--- movies.js
+++ movies.js
@@ -1,98 +1,12 @@
 const express = require('express');
 const axios = require('axios');
 const puppeteer = require('puppeteer');
-const nodemailer = require('nodemailer');
-const translatte = require('translatte');
-const request = require('request');
 const cheerio = require('cheerio');
-const TikTokScraper = require('tiktok-scraper');
 const reqToken = 'AAAAAAAAAAAAAAAAAAAAAKt9EQEAAAAA%2B1rwbmpXwSkxSLPTQFa5An9FxAs%3DVxvx2aYJEEI4l5sDNyHMtHa1rdlwpAtRHCdqhSMP16fzVHES4H';
 
 
-
-const options = {
-  // Number of posts to scrape: {int default: 20}
-  number: 50,
-
-  // Set proxy {string[] | string default: ''}
-  // http proxy: 127.0.0.1:8080
-  // socks proxy: socks5://127.0.0.1:8080
-  // You can pass proxies as an array and scraper will randomly select a proxy from the array to execute the requests
-  proxy: '',
-
-  // Set to {true} to search by user id: {boolean default: false}
-  by_user_id: false,
-
-  // How many post should be downloaded asynchronously. Only if {download:true}: {int default: 5}
-  asyncDownload: 5,
-
-  // How many post should be scraped asynchronously: {int default: 3}
-  // Current option will be applied only with current types: music and hashtag
-  // With other types it is always 1 because every request response to the TikTok API is providing the "maxCursor" value
-  // that is required to send the next request
-  asyncScraping: 3,
-
-  // File path where all files will be saved: {string default: 'CURRENT_DIR'}
-  filepath: `CURRENT_DIR`,
-
-  // Custom file name for the output files: {string default: ''}
-  fileName: `CURRENT_DIR`,
-
-  // Output with information can be saved to a CSV or JSON files: {string default: 'na'}
-  // 'csv' to save in csv
-  // 'json' to save in json
-  // 'all' to save in json and csv
-  // 'na' to skip this step
-  filetype: `na`,
-
-  // Set custom headers: user-agent, cookie and etc
-  // NOTE: When you parse video feed or single video metadata then in return you will receive {headers} object
-  // that was used to extract the information and in order to access and download video through received {videoUrl} value you need to use same headers
-  headers: {
-      'User-Agent': "BLAH",
-      Referer: 'https://cors-anywhere.herokuapp.com/https://www.tiktok.com/',
-      Cookie: `tt_webid_v2=68dssds`,
-  },
-  
-  // Download video without the watermark: {boolean default: false}
-  // Set to true to download without the watermark
-  // This option will affect the execution speed
-  noWaterMark: false,
-  
-  // Create link to HD video: {boolean default: false}
-  // This option will only work if {noWaterMark} is set to {true}
-  hdVideo: false,
-
-  // verifyFp is used to verify the request and avoid captcha
-  // When you are using proxy then there are high chances that the request will be 
-  // blocked with captcha
-  // You can set your own verifyFp value or default(hardcoded) will be used
-  verifyFp: ''
-};
-
 const route = express.Router();
 
-const movies = [{
-    id: 1,
-    name: 'Star Wars'
-  },
-  {
-    id: 2,
-    name: 'Star Trek'
-  },
-  {
-    id: 3,
-    name: 'Toy Story'
-  },
-  {
-    id: 4,
-    name: 'Transformers'
-  },
-  {
-    id: 5,
-    name: 'Terminator'
-  }
-]
 
 
 route.post('/api/twitter', (req,res) => {
@@ -122,26 +36,27 @@ route.post('/api/twitter', (req,res) => {
 
 
 
-route.post('/api/youtube', async (req, res) => {
+route.post('/api/linkedin', async (req, res) => {
   // console.log(req.body.username);
-  request('https://youtube.com/c/AmitTiwari/about', async function (error, response, html) {
-    if (!error && response.statusCode == 200) {
-      var $ = cheerio.load(html);
-      // let title = $("body div.share-native-video").html();
-      // // console.log(abc);
-      let title = $("body #masthead-container").text()
-      console.log(title);
-      // const reg = /[<>]/g;
-      // const reg2 = /&quot;/g
-      // let abc = title.replace(reg);
-      // let myHtml = abc.substr(abc.indexOf('[') + 1, abc.indexOf(']', 40) - 72).split(',')[0];
-      // const url = myHtml.substr(myHtml.indexOf('http'), myHtml.length - 15);
-      // console.log(typeof myHtml);
-      res.send({
-        video_url: title
-      });
-    }
-  });
+  let url = req.body.url
+  axios.get(url)
+  .then(data => {
+
+    var $ = cheerio.load(data.data);
+    let title = $(".share-native-video__node").attr('data-sources');
+    console.log(title);
+    res.status(200).send({
+      sc: 1,
+      video: title?JSON.parse(title):'err'
+    });
+  })
+  .catch(error => {
+    res.status(400).send({
+      sc: 2,
+      data: error
+    });
+  })
+
 
 })
 
